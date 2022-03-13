108795b36c8577d541bc76c4dc3f81979663e143
Buedenbender <b_buedenbender@yahoo.de>
2022-03-13 23:27:49 +0100

    * Implemented basic first unit tests * Added the news.md for a change log of the package

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# datscience 0.2.1
+
+* Modularized the main file datscience.R into small files for every function
+* Implemented basic first unit tests
+
 # datscience 0.1.2
 
 * Added a `NEWS.md` file to track changes to the package.
---
+++ tests/testthat/test_get_number_of_decimals.R
@@ -0,0 +1,6 @@
+test_that("get_number_of_decimals returns an positive integer (natural number)
+when given a positive integer (natural number, e.g., sample size)", {
+  expect_equal(get_number_of_decimals(sample.int(99,1)), 0)
+  expect_equal(get_number_of_decimals(sample(c(101:1000),1)), 1)
+  expect_equal(get_number_of_decimals(sample(c(1001:9999),1)), 2)
+})
