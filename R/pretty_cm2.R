d1fecce0c6b07f9196441969d026929fc0532639
Buedenbender <b_buedenbender@yahoo.de>
2022-05-19 17:16:41 +0200

    Added two options to pretty_cm diag to change the diagonal and tile for the content (inscription) on the tiles.

--- R/pretty_cm2.R
+++ R/pretty_cm2.R
@@ -14,7 +14,10 @@ utils::globalVariables(".")
 #' @param color_grad Pole of color gradient to use for the tiles, Default: c(alpha("yellowgreen", 0.4), alpha("springgreen3", 0.85))
 #' @param midpoint Manually setting a middle point in percentage for the color scale, Default: 50
 #' @param hideZero Hide tiles with 0 percentage, Default: FALSE
-#' @param ord Order of the factor levels to display (if you want to change it manually for the plot), Default: NA
+#' @param ord Order of the factor levels to display (if you want to change it manually for the plot), Default: NA,
+#' @param diag Orientation of the diagonal (sensitivities), possible values diag = "r" or "reverse"
+#' @param tile Character, either "p" or "prop" for proportion | "f" or "freq" for frequency | "b" or "both" for both.
+#' if character is not recognized or missing -> Default: "both"
 #' @param plot Shall the output also be plotted? Default: TRUE
 #' @return ggplot2 object - visualization of the confusion matrix.
 #' @author Björn Büdenbender
@@ -31,7 +34,7 @@ utils::globalVariables(".")
 #' }
 #' }
 #' @rdname pretty_cm
-#' @importFrom dplyr group_by mutate ungroup
+#' @importFrom dplyr group_by mutate ungroup case_when
 #' @importFrom rlang .data
 #' @import ggplot2
 #' @export
@@ -44,45 +47,71 @@ pretty_cm <- function(cm,
                         alpha("springgreen3", 0.85)
                       ),
                       midpoint = 50, hideZero = FALSE, ord = NA,
+                      diag = c("r","reverse"),
+                      tile = c("both","b","prop","p","freq","f"),
                       plot = TRUE) {
 
   # PREPARATION OF THE DATA MATRIX
+  cm_d <- as.data.frame(cm$table)
+  labels <- names(cm_d)
   # extract the confusion matrix values as data.frame
-  cm_d <- as.data.frame(cm$table) %>%
+  cm_d <- cm_d %>%
     # Create the proportion of the rowsum (in the diagonal it is sensitivity)
-    dplyr::group_by(.data$Reference) %>%
-    dplyr::mutate(rowsum_ref = sum(.data$Freq)) %>%
+    dplyr::group_by(.data[[labels[2]]]) %>%
+    dplyr::mutate(rowsum_ref = sum(.data[["Freq"]])) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(
-      Prop = round(.data$Freq / .data$rowsum_ref * 100, 1),
-      Prop_t = paste0(.data$Prop, "%")
+      Prop = round(.data[["Freq"]] / .data$rowsum_ref * 100, 1),
+      Prop_t = paste0(.data[["Prop"]], "%")
     )
 
   # order for the CM
   if (missing(ord)) {
-    ord <- levels(cm_d$Reference)
+    ord <- levels(cm_d[[labels[2]]])
+  } else if (length(ord)==1){
+    if(ord == "r" | ord == "reverse") ord <- levels(util_rev_fac(cm_d[[labels[2]]]))
   }
 
   # Change order of the y Axis
-  cm_d$Reference <- factor(cm_d$Reference,
+  cm_d[[labels[2]]] <- factor(cm_d[[labels[2]]],
     levels = ord
   )
-  cm_d$Prediction <- factor(cm_d$Prediction,
-    levels = ord
-  )
+  if(missing(diag) | length(diag) != 1){
+    cm_d[[labels[1]]] <- factor(cm_d[[labels[1]]], levels = ord)
+  } else{
+    if (diag == "r" | diag == "reverse") {
+      cm_d[[labels[1]]] <- util_rev_fac(factor(cm_d[[labels[1]]],
+                                               levels = ord))
+    } else     cm_d[[labels[1]]] <- factor(cm_d[[labels[1]]], levels = ord)
+  }
 
-  cm_d$ndiag <- cm_d$Prediction != cm_d$Reference # Not the Diagonal
-  cm_d$diag <- cm_d$Prediction == cm_d$Reference # Get the Diagonal
+
+
+  cm_d$ndiag <- cm_d[[labels[1]]] != cm_d[[labels[2]]] # Not the Diagonal
+  cm_d$diag <- cm_d[[labels[1]]] == cm_d[[labels[2]]] # Get the Diagonal
   if (hideZero) {
     cm_d[cm_d == "0%"] <- NA # Replace 0 with NA for white tiles
     cm_d[cm_d == 0] <- NA # Replace 0 with NA for white tiles
   }
 
-  cm_d$Reference <- util_rev_fac(cm_d$Reference)
+  cm_d[[labels[2]]] <- util_rev_fac(cm_d[[labels[2]]])
   cm_d$ref_Freq <- cm_d$Freq * ifelse(is.na(cm_d$diag), -1, 1)
 
+  # Setting inscription of the tiles
+  if(!missing(tile)){
+    tile_content <- dplyr::case_when(
+      tile == "f" | tile == "freq"  ~ as.character(cm_d$Freq),
+      tile == "p" | tile == "prop"  ~ as.character(cm_d$Prop_t),
+      TRUE ~ paste0(cm_d$Freq," (",cm_d$Prop_t,")")
+    )
+  } else{
+    tile_content <- paste0(cm_d$Freq," (",cm_d$Prop_t,")")
+  }
+
+
+  # === Creating the Plot ===
   cm_d_p <- ggplot(data = cm_d, aes(
-    x = .data$Prediction, y = .data$Reference,
+    x = .data[[labels[1]]], y = .data[[labels[2]]],
     fill = .data$Freq
   )) +
     scale_x_discrete(position = "top") +
@@ -96,7 +125,7 @@ pretty_cm <- function(cm,
       midpoint = midpoint,
       na.value = "black"
     ) +
-    geom_text(aes(label = .data$Prop_t), color = "black", size = 5) +
+    geom_text(aes(label = tile_content), color = "black", size = 5) +
     theme_light() +
     theme(
       panel.grid.major = element_blank(),
@@ -121,7 +150,7 @@ pretty_cm <- function(cm,
       )
     ) +
     guides(fill = "none") +
-    ylab("Reference")
+    ylab(labels[2])
   if (plot) plot(cm_d_p)
   return(cm_d_p)
 }
